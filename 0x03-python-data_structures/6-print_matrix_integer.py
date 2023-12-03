3909fd8ce4c103971507c59e190b594c8a6e87ab
Asma22334585 <guesmihadil0@gmail.com>
2023-12-03 12:43:46 -0800

    my commit

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for sm in matrix:
+        if len(sm) == 0:
+            print()
+        for x in range(len(sm)):
+            print("{:d}".format(sm[x]),
+                    end="" if x is len(sm) - 1 else " ")
