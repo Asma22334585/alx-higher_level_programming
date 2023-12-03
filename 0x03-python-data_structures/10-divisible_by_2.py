f48777cdc8832c900c464909aaa3e0fa4cb5cf6d
Asma22334585 <guesmihadil0@gmail.com>
2023-12-03 13:18:01 -0800

    my commit

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,7 +4,7 @@ def divisible_by_2(my_list=[]):
         return None
     div_list = []
     for x in my_list:
-        if (x % 2) ==0:
+        if (x % 2) == 0:
             div_list.append(True)
         else:
             div_list.append(False)
