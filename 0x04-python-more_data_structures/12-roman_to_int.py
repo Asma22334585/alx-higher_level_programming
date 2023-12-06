07ee9466f44c3438982709159bbe24ee0f99a488
Asma22334585 <guesmihadil0@gmail.com>
2023-12-06 12:29:16 -0800

    my commit

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     if roman_string is None or type(roman_string) != str:
         return 0
     s = 0
-    n = 0
+    n = [x[i] for i in roman_string] + [0]
     x = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C':100, 'D':500, 'M':1000}
     for y in reversed(roman_string):
         n = x[y]
