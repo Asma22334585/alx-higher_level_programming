c4a17d7c9a53624638436af0c2c59d14990489c7
Asma22334585 <guesmihadil0@gmail.com>
2023-12-19 01:24:56 -0800

    my commit

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,8 +6,9 @@ class Square:
       """Defines a square."""
 
      def __init__(self, size):
-           """Constructor.
-        Args:
-            size: length of side of the square.
+         """Constructor.
+
+         Args:
+             size: length of side of the square.
         """
         self.__size = size
