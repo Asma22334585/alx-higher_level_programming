a31b0c2037a1beef4f2ae336501a6425f32ab4f4
Asma22334585 <guesmihadil0@gmail.com>
2024-01-05 12:01:50 -0800

    my commit

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """defines LockedClass"""
 
+
 class LockedClass:
-    """except if the new instance attribute is called first_name"""
+    """
+    except if the new instance attribute is called first_name
+    """
 
      __slots__ = ['first_name']
