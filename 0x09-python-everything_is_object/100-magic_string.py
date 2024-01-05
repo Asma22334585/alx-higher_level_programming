812f0614d2b71e284040d79a1d7e2ed5b9a55ea2
Asma22334585 <guesmihadil0@gmail.com>
2024-01-05 12:13:18 -0800

    my commit

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(l=[]):
-    l += ["BestSchool"]
-    return ", ".join(l)
+def magic_string():
+    magic_string.h = getattr(magic_string, 'h', 0) + 1
+    return ", ".join(["BestSchool" for x in range(magic_string.h)])
