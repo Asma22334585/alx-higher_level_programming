56ea5bc9e6a54fa12d529d1e20a935af24f754e4
Asma22334585 <guesmihadil0@gmail.com>
2024-01-09 13:00:33 -0800

    my commit

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -9,14 +9,12 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    lenght = [[0 for x in range(i + 1)] for i in range(n)]
-    lenght[0] = [1]
-
-    for i in range(1, n):
-        lenght[i][0] = 1
-        for j in range(1, i + 1):
-            if j < len(l[i - 1]):
-                lenght[i][j] = lenght[i - 1][j - 1] + lenght[i - 1][j]
-            else:
-                lenght[i][j] = lenght[i - 1][0]
-    return lenght
+    tri = [[1]]
+    while len(tri) != n:
+        x = tri[-1]
+        tmp = [1]
+        for i in range(len(x) - 1):
+            tmp.append(x[i] + x[i + 1])
+        tmp.append(1)
+        tri.append(tmp)
+    return tri
