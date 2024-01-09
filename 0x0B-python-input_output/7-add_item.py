c93ea632509c3c4982843a869a2ed5ee581e9cf2
Asma22334585 <guesmihadil0@gmail.com>
2024-01-09 13:41:00 -0800

    my commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-"""task 7"""
-
+"""Task 7"""
 import sys
-import os.path
-import json
 
-save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
+if __name__ == "__main__":
+    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+    load_from_json_file = \
+        __import__("6-load_from_json_file").load_from_json_file
+    filename = "add_item.json"
 
-load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
+    try:
+        list_items = load_from_json_file(filename)
+    except FileNotFoundError:
+        list_items = []
 
-myfile = "add_item.json"
-json = []
+    list_items.extend(sys.argv[1:])
 
-if os.path.exists(myfile):
-    json = load_from_json_file(myfile)
-
-for arg in range(1, len(sys.argv)):
-    json.append(sys.argv[arg])
-
-save_to_json_file(json, myfile)
+    save_to_json_file(list_items, filename)
