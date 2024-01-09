cde2195457f3890cd933298709d89d04338ebabc
Asma22334585 <guesmihadil0@gmail.com>
2024-01-09 13:50:07 -0800

    my commit

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
-"""task 13"""
+"""Task 13"""
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """inserts a line of text to a file,
-    after each line containing a specific string"""
+    """
+    inserts a line of text to a file,
+    after each line containing a specific string
+    """
+    nlist = "\n" + new_string
 
-    with open(filename, 'r', encoding='utf-8') as f:
-        line_list = []
-        while True:
-            line = f.readlines()
-            if line == "":
-                break
-            line_list.append(line)
+    mylist = []
+
+    with open(filename, "r", encoding="UTF-8") as f:
+        for line in f:
             if search_string in line:
-                line_list.append(new_string)
-    
-    with open(filename, 'w', encoding='utf-8') as f:
-        f.writelines(line_list)
+                line = line.replace("\n", nlist)
+            mylist.append(line)
+        x = "".join(txtlist)
+
+    with open(filename, "w", encoding="UTF-8") as f:
+        f.write(x)
