4f21f2e35a53cba0a84eb4322eaaf933bdba595a
Asma22334585 <guesmihadil0@gmail.com>
2024-01-09 13:37:24 -0800

    my commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,9 +3,10 @@
 
 import sys
 import os.path
-import sys
+import json
 
 save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
+
 load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
 
 myfile = "add_item.json"
