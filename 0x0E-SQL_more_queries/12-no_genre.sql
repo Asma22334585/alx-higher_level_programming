5ee08bfa32456d6c5c0108f31ad739b22b1a9662
Asma22334585 <guesmihadil0@gmail.com>
2024-02-14 13:40:11 -0800

    my commit

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,7 +1,7 @@
 -- Import the database dump from hbtn_0d_tvshows to your MySQL server
 -- lists all shows contained in hbtn_0d_tvshows without a genre linked.
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_show_genres
-RIGHT JOIN tv_show ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.show_id IS NULL
-ORDER BY tv_shows.title, tv_show_genres.genre_id;;
+SELECT shows.title AS title, genres.genre_id AS genre_id
+FROM tv_shows AS shows
+LEFT JOIN tv_show_genres AS genres ON shows.id = genres.show_id
+WHERE genre_id IS NULL
+ORDER BY title ASC, genre_id ASC;
