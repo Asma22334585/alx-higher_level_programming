7c4306856f10ea525ced8cabc7346bc7a4ca1d4f
Asma22334585 <guesmihadil0@gmail.com>
2024-03-11 16:35:03 -0700

    my commit

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,7 +4,7 @@ const num = Math.floor(+process.argv[2]);
 if (isNaN(num)) {
   console.log('Missing size');
 } else {
-	for (let i = 0; i < num; i++) {
-	  console.log('X'.repeat(num));
+  for (let i = 0; i < num; i++) {
+    console.log('X'.repeat(num));
 	}
 }
