079e74289e7ed09d74e9a338eb757ccf49ee4331
Asma22334585 <guesmihadil0@gmail.com>
2024-03-12 15:45:28 -0700

    my commit

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const initialList = require('./100-data.js').list;
-const newList = initList.map((number, index) => number * index);
+const newList = initialList.map((number, index) => number * index);
 
 console.log(initialList);
 console.log(newList);
