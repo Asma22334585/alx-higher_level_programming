1d30818747b3ad937cf21ab276b343caf770e633
Asma22334585 <guesmihadil0@gmail.com>
2024-03-12 14:25:42 -0700

    my commit

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const SquareAS = require('./5-square');
+
+class Square extends SquareAS {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let x = '';
+      for (let j = 0; j < this.width; j++) {
+        x += c;
+      }
+      console.log(x);
+    }
+  }
+}
+
+module.exports = Square;
