14c30e115a8a1191bf4710869b31e66251c2e434
Asma22334585 <guesmihadil0@gmail.com>
2024-04-02 10:34:47 -0700

    my commit

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -16,11 +16,12 @@ def add_integer(a, b=98):
         the addition of a and b
     """
 
-    if type(a) not in (int, float):
+    if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError('a must be an integer')
-    if type(b) not in (int, float):
+    if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError('b must be an integer')
-    return int(a) + int(b)
+    return (int(a) + int(b))
+
 
 if __name__ == "__main__":
     import doctest
--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -51,6 +51,11 @@ class TestMaxInteger(unittest.TestCase):
         """Unittest for max_integer([..])"""
         self.assertEqual(max_integer([23]), 23)
 
+    def test_max_at_begginning(self):
+        """Test a list with a beginning max value."""
+        max_at_beginning = [4, 3, 2, 1]
+        self.assertEqual(max_integer(max_at_beginning), 4)
+
 
 if __name__ == '__main__':
     unittest.main()
