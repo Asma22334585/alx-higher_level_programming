5e19afaaf9be6891c7f2616bf3df8b867f16ce72
Asma22334585 <guesmihadil0@gmail.com>
2024-04-23 04:59:04 -0700

    my commit

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const request = require('request');
+const id = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/';
+request.get(url + id, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  }
+  for (const x of JSON.parse(body).characters) {
+    request.get(x, (error, response, bodyn) => {
+      if (error) {
+        console.log(error);
+      }
+      console.log(JSON.parse(bodyn).name);
+    });
+  }
+});
