6d35a5004c1bbd4546721f0c2c66d7d102d89173
Asma22334585 <guesmihadil0@gmail.com>
2024-04-23 05:00:58 -0700

    my commit

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 const id = process.argv[2];
-const url = 'https://swapi-api.alx-tools.com/';
+const url = 'https://swapi-api.alx-tools.com/api/films/';
 request.get(url + id, (error, response, body) => {
   if (error) {
     console.log(error);
